e6210f7a28d8a68003ea9fbd13bbf436df0aaef1
Patrick Ineichen <github@frokonet.ch>
2024-09-09 18:53:40 +0200

    feat(docs): prepare project

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,28 +1,33 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import simplePlantUML from '@akebifiky/remark-simple-plantuml';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  outDir: '../dist/docs',
+  markdown: {
+    remarkPlugins: [simplePlantUML]
+  },
+  integrations: [
+    starlight({
+      title: 'nx-monorepo',
+      social: {
+        github: 'https://github.com/froko/nx-monorepo'
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', slug: 'guides/example' }
+          ]
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' }
+        }
+      ]
+    })
+  ]
 });
